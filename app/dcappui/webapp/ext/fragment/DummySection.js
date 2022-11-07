dd5ce6cf2177431b397b01834a69f44e47e14134
Tristan <tristan.heisecke@implico.com>
2022-11-07 16:03:21 +0100

    try out macro control

--- app/dcappui/webapp/ext/fragment/DummySection.js
+++ app/dcappui/webapp/ext/fragment/DummySection.js
@@ -1,15 +1,30 @@
 sap.ui.define([
     "sap/m/MessageToast"
-], function(MessageToast) {
+], function (MessageToast) {
     'use strict';
 
     return {
         onInit: function () {
-          console.log("I was here");  
-        },
-        onPress: function() {
+            console.log("I was here");
             debugger
-            this.byId("dcappui::HeadersObjectPage--fe::table::items::LineItem::items::Table");
+        },
+        onPress: function () {
+            let macroTable = this.byId("logTab")
+            macroTable.setVisible(true);
+            let oHeader = macroTable.getBindingContext().getObject();
+            let tableDefinition = macroTable.getProperty("tableDefinition")
+            
+            //Hacky
+            //this.byId("dcappui::HeadersObjectPage--fe::table::items::LineItem::items-innerTable").getColumns()[0].setVisible(false)
+            debugger
+            if (false) {
+                macroTable.addMessage({
+                    description: "Some description",
+                    message: "Some Error",
+                    persistent: false,
+                    type: sap.ui.core.MessageType.Error
+                });
+            }
             MessageToast.show("Custom handler invoked.");
         }
     };
