dfc2e3976515316849da00985188612627f1bf36
Tristan <tristan.heisecke@implico.com>
2023-01-12 15:13:08 +0100

    Test aggregation & Messages

--- app/dcappui/webapp/ext/fragment/DummySection.js
+++ app/dcappui/webapp/ext/fragment/DummySection.js
@@ -13,16 +13,31 @@ sap.ui.define([
             macroTable.setVisible(true);
             let oHeader = macroTable.getBindingContext().getObject();
             let tableDefinition = macroTable.getProperty("tableDefinition")
-            
-            //Hacky
-            //this.byId("dcappui::HeadersObjectPage--fe::table::items::LineItem::items-innerTable").getColumns()[0].setVisible(false)
+
+            let mappedTableDefinition = tableDefinition.columns.map(c => {
+                c.availability = "Adaptation";
+                return c;
+            })
+            // mappedTableDefinition[0].availability = "Default";
+            // macroTable.setProperty("tableDefinition", mappedTableDefinition); // not documented, does not apply
+
             debugger
-            if (false) {
+            let content = macroTable.getAggregation("content");
+            let columns = content.getAggregation("columns");
+            columns.pop();
+            //content.setAggregation("columns", columns.pop());
+            content.addAggregation("columns", columns.pop()); //works but kills personalization
+            macroTable.invalidate();
+
+            //Hacky (operated on default table)
+            //this.byId("dcappui::HeadersObjectPage--fe::table::items::LineItem::items-innerTable").getColumns()[0].setVisible(false)
+            //this.byId("dcappui::HeadersObjectPage--fe::CustomSubSection::DummySection--logTab-innerTable").getColumns()[0].setVisible(false)
+            if (true) {
                 macroTable.addMessage({
                     description: "Some description",
                     message: "Some Error",
                     persistent: false,
-                    type: sap.ui.core.MessageType.Error
+                    type: sap.ui.core.MessageType.Information
                 });
             }
             MessageToast.show("Custom handler invoked.");
